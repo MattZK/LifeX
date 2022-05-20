ac37e69d0baa2932dd096e592def11843df6f9bb
Matthias Willemsen <matthias@districtthree.be>
2022-05-20 19:20:36 +0200

    Added packages

---
+++ packages/app-lifex-expenses/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-lifex-expenses',
+  webDir: '../../dist/packages/app-lifex-expenses',
+  bundledWebRuntime: false,
+};
+
+export default config;
